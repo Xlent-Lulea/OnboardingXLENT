e850355d297798d9216b0db9c493df4b2ee12323
xlentsund <131262031+xlentsund@users.noreply.github.com>
2023-09-14 09:29:45 +0200

    selectedPerson$ global variabel

--- src/app/Components/expansion-panel/expansion-panel.component.ts
+++ src/app/Components/expansion-panel/expansion-panel.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit, Input } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
 import { Task, TaskType } from '../../models/task.interface';
-import { Input } from '@angular/core';
 import { Person } from 'src/app/models/task.interface';
 import { MatAccordion } from '@angular/material/expansion';
-
+import { PersonService } from 'src/app/services/person.service';
+import { Observable } from 'rxjs';
 
 /**
  * @title Accordion with expand/collapse all toggles
@@ -14,108 +14,33 @@ import { MatAccordion } from '@angular/material/expansion';
   templateUrl: 'expansion-panel.component.html',
   styleUrls: ['expansion-panel.component.scss'],
 })
-export class ExpansionPanelComponent implements OnInit {
-  constructor(private taskService: TaskService,
-              private selectedPersonService: SelectedPersonService,
-              private personService: PersonService) {}
+export class ExpansionPanelComponent {
+  public selectedPerson$: Observable<Person | null> = this.personService.selectedPerson$;
+
   @ViewChild(MatAccordion) accordion!: MatAccordion;
   tasksByType: { [key in TaskType]?: Task[] } = {};
   taskTypes: TaskType[] = Object.values(TaskType).filter(
     (value) => typeof value === 'string'
   ) as TaskType[];
+
   @Input() selectedTaskType: string = '';
-  // ... your interface and enum definitions ...
+  @Input() selectedPerson: Person | null = null;
+
+  constructor(
+    private taskService: TaskService,
+    private personService: PersonService
+  ) {}
+
+  ngOnInit() {
+    console.log('Task Types:', this.taskTypes);
+    this.selectedPerson$.subscribe(person => {
+        console.log('Selected Person:', person);
+    });
+}
 
   shouldShowDivider(task: Task, index: number, tasks: Task[]): boolean {
     if (index === 0) return false;
     return tasks[index - 1].taskType === task.taskType;
-  }
-
-  ngOnInit() {
-    this.subscribeToSelectedPersonId();
-  }
-
-  selectedPerson: Person = {
-    id: 3,
-    name: "Test",
-    email: "Test123@sdaad.com",
-    active: true,
-    tasks: [
-      {
-        urltitle: "Introduction",
-        taskType: TaskType.WELCOME,
-        url: "https://example.com/introduction",
-        completed: false,
-        description: "GjHej hjjHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hsasadj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj hejHej hj he hej",
-        active: true
-      },
-      {
-        urltitle: "2 Introduction",
-        taskType: TaskType.WELCOME,
-        url: "https://example.com/introduction",
-        completed: false,
-        description: "2 Get introduced to the plasadsadadstform.",
-        active: true
-      },
-      {
-        urltitle: "Meet Your Buddy",
-        taskType: TaskType.BUDDY,
-        url: "https://example.com/meetbuddy",
-        completed: false,
-        description: "Meet and chat with your assigned buddyadsdsaasddasdas.",
-        active: true
-      },
-      {
-        urltitle: "Recruitment Training",
-        taskType: TaskType.START,
-        url: "https://example.com/recruitment",
-        completed: false,
-        description: "Understand the recruitment processadsassdadsadsadsadsadsdsaadsdsdsadsadass.",
-        active: true
-      },
-      {
-        urltitle: "Admin Intro",
-        taskType: TaskType.ADMIN,
-        url: "https://example.com/admin-intro",
-        completed: false,
-        description: "Lär dig om anställningsprocessen.sssdasdasdasdsdsadssdsadasdasdsdasdas",
-        active: true
-      },
-      {
-        urltitle: "Digital Platform",
-        taskType: TaskType.DIGITAL_SETUP,
-        url: "https://example.com/digital-setup",
-        completed: false,
-        description: "Sätt upp din digitala arbetsplaasddsadsadsadsdasdsasaddsadsaadsdsats.",
-        active: true
-      },
-      {
-        urltitle: "Konsultrollen Introduktion",
-        taskType: TaskType.CONSULTANT,
-        url: "https://example.com/konsultrollen",
-        completed: false,
-        description: "Förstå din roll som konasdssadadsdasdsadsadasdsadassadadsdssult.",
-        active: true
-      },
-      {
-        urltitle: "Avslutning av Onboarding",
-        taskType: TaskType.FINISH,
-        url: "https://example.com/avslut",
-        completed: false,
-        description: "Slutför onboarding-processensdsadsasddsdsadasdadsadsaddasd.",
-        active: true
-      }
-    ]
-  };
-
-
-
-  ngOnInit(): void {
-
-    // const personId = 1// ... hämta personId du vill hämta uppgifter för ...
-    // for (let type of this.taskTypes) {
-    //   this.fetchTasksByType(type, personId);
-    // }
   }
 
   fetchTasksByType(taskType: TaskType, personId: number): void {
--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -1,5 +1,90 @@
+// person.service.ts
 
-getPersonId(): BehaviorSubject<string> {
-  return this.personId;
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Person } from '../models/task.interface';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class PersonService {
+  private activePersonsSubject = new BehaviorSubject<Person[]>([]);
+  public activePersons$ = this.activePersonsSubject.asObservable();
+
+  private selectedPersonSubject = new BehaviorSubject<Person | null>(null);
+  public selectedPerson$ = this.selectedPersonSubject.asObservable();
+
+  public personsUrl = `${window.location.protocol}//${window.location.hostname}:8081`;
+
+  constructor(private http: HttpClient) {
+    this.refreshActivePersons();
+  }
+
+  refreshActivePersons(): void {
+    this.getActivePersons().subscribe();
+  }
+
+  getAllPersons(): Observable<Person[]> {
+    return this.http.get<Person[]>(`${this.personsUrl}/persons`).pipe(
+      tap((persons) => this.activePersonsSubject.next(persons))
+    );
+  }
+
+  getPerson(id: number): Observable<Person> {
+    return this.http.get<Person>(`${this.personsUrl}/person/${id}`).pipe(
+      tap((person) => {
+      localStorage.setItem('personId', '' + id);
+      this.selectedPersonSubject.next(person);
+    })
+    );
+  }
+
+  getPersons(): Observable<Person[]> {
+    const url = `${this.personsUrl}/persons`;
+    return this.http.get<Person[]>(url).pipe(
+      tap((persons) => this.activePersonsSubject.next(persons))
+    );
+  }
+
+  getActivePersons(): Observable<Person[]> {
+    const url = `${this.personsUrl}/activePersons`;
+    return this.http.get<Person[]>(url)
+      .pipe(tap((persons) => this.activePersonsSubject.next(persons)));
+  }
+
+  createPerson(person: Person): Observable<Person> {
+    const url = `${this.personsUrl}/createPerson`;
+    return this.http.post<Person>(url, person);
+  }
+
+  updatePerson(person: Person): Observable<Person> {
+    const url = `${this.personsUrl}/updatePerson`;
+    return this.http.post<Person>(url, person);
+  }
+
+  addTask(personId: number, task: any[]): Observable<Person> {
+    const url = `${this.personsUrl}/${personId}/tasks`;
+    return this.http.post<Person>(url, task);
+  }
+
+  removeTask(personId: number, taskId: number): Observable<Person> {
+    const url = `${this.personsUrl}/${personId}/tasks/${taskId}`;
+    return this.http.delete<Person>(url);
+  }
+
+  deactivatePerson(personId: number): Observable<any> {
+    return this.http.put(`${this.personsUrl}/person/${personId}/deactivate`, {});
+  }
+
+  activatePerson(personId: number): Observable<any> {
+    return this.http.put(`${this.personsUrl}/person/${personId}/activate`, {});
+  }
+
+  deletePerson(personId: number): Observable<any> {
+    return this.http.delete(`${this.personsUrl}/person/${personId}`, {});
+  }
 }
-}
+
