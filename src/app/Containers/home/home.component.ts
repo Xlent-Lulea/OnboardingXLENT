ba696747617cfc87dadc90bc62f31d0bc068650c
xlentsund <131262031+xlentsund@users.noreply.github.com>
2023-10-05 18:42:43 +0200

    total-progress klar

--- src/app/Containers/home/home.component.ts
+++ src/app/Containers/home/home.component.ts
@@ -8,12 +8,29 @@ import { Observable, map } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
+
 export class HomeComponent {
   selectedPerson$: Observable<Person | null> =
     this.personService.selectedPerson$;
   tasks$: Observable<Task[]> = this.selectedPerson$.pipe(
     map((person) => person?.taskEntities || [])
   );
+  isAllTasksCompleted = false; // Initialize as false
 
   constructor(private personService: PersonService) {}
+
+  calculateOverallTaskProgress(): number {
+    let totalTasksCount = 0;
+    let completedTasksCount = 0;
+
+    this.tasks$.subscribe((tasks) => {
+      totalTasksCount = tasks.length;
+      completedTasksCount = tasks.filter((task) => task.completed).length;
+
+      // Check if all tasks are completed
+      this.isAllTasksCompleted = completedTasksCount === totalTasksCount;
+    });
+
+    return totalTasksCount > 0 ? (completedTasksCount / totalTasksCount) * 100 : 0;
+  }
 }
