2b029f6620734e25df2cae7d75f5d5b467ce0f68
xlentsund <131262031+xlentsund@users.noreply.github.com>
2023-10-19 15:10:57 +0200

    test succeed

--- src/app/Components/manage-tasktypes/manage-tasktypes.component.spec.ts
+++ src/app/Components/manage-tasktypes/manage-tasktypes.component.spec.ts
@@ -1,15 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { ManageTasktypesComponent } from './manage-tasktypes.component';
 
 describe('ManageTasktypesComponent', () => {
   let component: ManageTasktypesComponent;
   let fixture: ComponentFixture<ManageTasktypesComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ManageTasktypesComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ManageTasktypesComponent],
+      imports: [FormsModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ManageTasktypesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
