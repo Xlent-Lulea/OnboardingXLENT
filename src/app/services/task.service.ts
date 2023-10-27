2f7e6caca62dbe3e331a77f6fd45cf9d1d1e908e
sohjla <sofia.hjort.labba@gmail.com>
2023-10-27 09:16:45 +0200

    Add user feedback

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Task } from '../models/task.interface';
 import { TaskType } from '../models/task-type.interface';
-
+import { SnackBarService } from './snack-bar-service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class TaskService {
 
   private tasksUrl = `${window.location.protocol}//${window.location.hostname}:8081`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private snackBarService: SnackBarService) { }
 
   getAll(): Observable<Task[]> {
     return this.http.get<Task[]>(`${this.tasksUrl}/tasks`);
@@ -23,11 +23,15 @@ export class TaskService {
       task.url = 'https://' + task.url;
     }
 
-    return this.http.post<Task>(`${this.tasksUrl}/tasks`, task);
+    return this.http.post<Task>(`${this.tasksUrl}/tasks`, task).pipe(
+      tap(() => this.snackBarService.show('Task sparad!'))
+    );
   }
 
   remove(taskId: number): Observable<void> {
-    return this.http.delete<void>(`${this.tasksUrl}/tasks/${taskId}`);
+    return this.http.delete<void>(`${this.tasksUrl}/tasks/${taskId}`).pipe(
+      tap(() => this.snackBarService.show('Task borttagen'))
+    );
   }
 
   getTypes(): Observable<TaskType[]> {
