1ebe9fe50cf4b3070173ba4eefe998b6011a516f
sohjla <sofia.hjort.labba@gmail.com>
2023-11-30 11:51:38 +0100

    Finalize content, hide scroll

--- src/app/Containers/knowledge/knowledge.component.ts
+++ src/app/Containers/knowledge/knowledge.component.ts
@@ -11,80 +11,16 @@ export class KnowledgeComponent {
 
   focusGroups = [
     {
-      heading: 'Rekrytering',
-      subheading: 'Ansvarig: Alexandra Rönnkvist',
-      tasks: [
-        '•	Ansvarig för rekryteringsprocessen',
-        '•	Samarbete med rekryteringsfirmor',
-        '•	Sammanhållande av rekryterings-processen för varje rekrytering',
-        '•	Planera för kommande rekryteringar, vem som skriver annons, vilka som ska delta på intervjuer m.m ',
-        '•	Ansvarar för pågående rekryteringar och beslut',
-        '•	Informerar övriga och tar in input kring beslut'
-      ]
-    },
-    {
-      heading: 'Ekonomi',
-      subheading: 'Ansvarig: Veronica Andersson',
-      tasks: [
-        '•	Håller ihop arbete med budget och prognoser',
-        '•	Ekonomisk uppföljning och rapportering till övriga inom XLU',
-        '•	Attest av fakturor',
-
-      ]
-    },
-    {
-      heading: 'Sälj och relationsbyggande ',
-      subheading: 'Ansvarig: Sofia Flodmark',
-      tasks: [
-        '•	Bevaka beläggning framåt',
-        '•	Bevaka ramavtal (nya och befintliga avrop)',
-        '•	Nätverksbyggande (delta på nätverksträffar, planera för egna arrangemang, representera etc)',
-        '•	Stötta konsulter i eget säljarbete (om konsult ej har full beläggning blir denne automatiskt med i fokusgrupp sälj)',
-        '•	Säljaktiviteter nya kunder'
-      ]
-    },
-    {
-      heading: 'Lokaler',
+      heading: 'Coach (valbar roll för de som är partner)',
       subheading: 'Ansvarig: Katja Perunka',
       tasks: [
-        '•	Upprätta och underhålla dokumentationen rörande lokaler och lösöre.',
-        '•	Företagets kontakt med de servicebolag XLENT Luleå har avtal/kontakt med.'
-      ]
-    },
-    {
-      heading: 'Onboarding',
-      subheading: 'Ansvarig: Emmy Valfridsson',
-      tasks: [
-        '•	Ansvarar för och utvecklar processen för att ta emot nyanställda',
-        '•	Initiera onboardingprocessen vid nyanställning enligt överenskommelse i dokumentation',
-        '•	Ansvara för samordning av årlig fotografering'
-      ]
-    },
-    {
-      heading: 'IT & telefoni',
-      subheading: 'Ansvarig: Christian Werme',
-      tasks: [
-        '•	Beställningar av utrustning kopplat till teknik såsom Telefoner, Datorer, teknisk utrustning till kontor.',
-        '•	Telefonabonnemang.',
-        '•	Vara behjälplig om någon har problem med teknik.'
-
-      ]
-    },
-    {
-      heading: 'Sociala aktiviteter (internt) (fokusgrupp)',
-      subheading: 'Ansvarig: August Wande',
-      tasks: [
-        '•	Ansvarig för rekryteringsprocessen',
-        '•	Samarbete med rekryteringsfirmor',
-        '•	Sammanhållande av rekryterings-processen för varje rekrytering',
-        '•	Planera för kommande rekryteringar, vem som skriver annons, vilka som ska delta på intervjuer m.m ',
-        '•	Ansvarar för pågående rekryteringar och beslut',
-        '•	Informerar övriga och tar in input kring beslut'
+        '•	Utbyter erfarenhet mellan coacherna',
+        '•	Utvecklar processen för att säkerställa att fånga upp medarbetarnas mående'
       ]
     },
     {
       heading: 'HR',
-      subheading: 'Ansvarig: Christian Wande',
+      subheading: 'Ansvarig: Christian Werme',
       tasks: [
         'Ta fram och utveckla processer för:',
         '•	Kultur/värderingar - Utvecklar beskrivning av XLU företagskultur och hur vi omsätter kulturen i praktiken',
@@ -95,32 +31,102 @@ export class KnowledgeComponent {
       ]
     },
     {
-      heading: 'Coach',
-      subheading: 'Ansvarig: Katja Perunka',
+      heading: 'Kosegruppa (Interna sociala aktiviteter)',
+      subheading: 'Ansvarig: August Wande',
       tasks: [
-        '•	Utbyter erfarenhet mellan coacherna',
-        '•	Utvecklar processen för att säkerställa att fånga upp medarbetarnas mående'
+        '•	Samla in önskemål om möjliga interna sociala aktiviteter',
+        '•	Planera och tidsätta när aktiviteter genomförs',
+        '•	Roliga aktiviteter',
+        '•	Kickoffer och konferenser'
       ]
     },
-
+    {
+      heading: 'Lokaler',
+      subheading: 'Ansvarig: Katja Perunka',
+      tasks: [
+        '•	Upprättar och underhålla dokumentationen rörande lokaler och lösöre',
+        '•	Företagets kontakt med de servicebolag XLENT Luleå har avtal/kontakt med',
+        '•	Genomgång lokaler med nyanställd'
+      ]
+    },
+    {
+      heading: 'Marknad',
+      subheading: 'Ansvarig: Sofia Flodmark',
+      tasks: [
+        '•	Marknadsföring av XLU',
+        '•	Delta på externa event'
+      ]
+    },
+    {
+      heading: 'Onboarding',
+      subheading: 'Ansvarig: Emmy Valfridsson',
+      tasks: [
+        '•	Ansvarar för och utvecklar processen för att ta emot nyanställda',
+        '•	Initierar onboardingprocessen vid nyanställning',
+        '•	Ansvarar för samordning av fotografering'
+      ]
+    },
+    {
+      heading: 'Rekrytering',
+      subheading: 'Ansvarig: Alexandra Rönnkvist',
+      tasks: [
+        '•	Ansvarar för rekryteringsprocessen',
+        '•	Samarbete med rekryteringsfirmor',
+        '•	Sammanhållande av rekryterings-processen för varje rekrytering',
+        '•	Planera för kommande rekryteringar, vem som skriver annons, vilka som ska delta på intervjuer m.m ',
+        '•	Ansvarar för pågående rekryteringar och beslut',
+        '•	Informerar övriga och tar in input kring beslut'
+      ]
+    },
+    {
+      heading: 'Sälj',
+      subheading: 'Ansvarig: Sofia Flodmark',
+      tasks: [
+        '•	Bevakar beläggning framåt',
+        '•	Bevakar ramavtal (nya och befintliga avrop)',
+        '•	Nätverksbyggande (delta på nätverksträffar, bygga relationer, planera för egna arrangemang, representera etc)',
+        '•	Stöttar konsulter i eget säljarbete',
+        '•	Säljaktiviteter nya kunder'
+      ]
+    },
   ];
-areaofResponsibility = [
-  {
-    heading: 'Studentambassadör',
-    subheading: 'Ansvarig: August Wande',
-    tasks: [
-      '•	Ansvarig LITA',
-      '•	Kontaktperson gentemot Universitet',
-      '•	Initiera Studentträffar',
-    ]
-  },
+  areaofResponsibility = [
+    {
+      heading: 'Ekonomi',
+      subheading: 'Ansvarig: Veronica Andersson',
+      tasks: [
+        '•	Håller ihop arbete med budget och prognoser',
+        '•	Ekonomisk uppföljning och rapportering till övriga inom XLU',
+        '•	Attest av fakturor',
+
+      ]
+    },
   {
     heading: 'Hälsoundersökning',
     subheading: 'Ansvarig: Emmy Valfridsson',
     tasks: [
-      '•	Att hälsoundersökning genomförs vartannat år'
+      '•	Ansvarar för att hälsoundersökning genomförs vartannat år'
     ]
-  },
+    },
+    {
+      heading: 'IT & telefoni',
+      subheading: 'Ansvarig: Christian Werme',
+      tasks: [
+        '•	Beställningar av utrustning kopplat till teknik såsom telefoner, datorer, teknisk utrustning till kontor',
+        '•	Telefonabonnemang',
+        '•	Vara behjälplig om någon har problem med teknik'
+
+      ]
+    },
+    {
+      heading: 'Studentambassadör',
+      subheading: 'Ansvarig: August Wande',
+      tasks: [
+        '•	Ansvarig LITA',
+        '•	Kontaktperson gentemot universitet',
+        '•	Initierar studentträffar',
+      ]
+    },
   {
     heading: 'VD, ledningsfrågor',
     subheading: 'Ansvarig: Veronica Andersson',
@@ -129,26 +135,10 @@ areaofResponsibility = [
       '•Deltagande på ledningsmöten och andra möten mellan bolag',
       '•Signerar avtal och genomför säkerhetsavtal',
       '•Säkerhetsansvarig XLU',
-      '•Huvudsaklig kontaktperson till/från andra XLENT-bolag samt VD XCG.',
-      '•Medverkan i rekryteringar i 2a intervju.',
+      '•Huvudsaklig kontaktperson till/från andra XLENT-bolag samt VD XCG',
       '•Månadsrapport XCG',
       '•Attest av fakturor',
       '•Godkännande av tidrapporter',
-    ]
-  },
-  {
-    heading: 'Medarbetarenkät',
-    subheading: 'Ansvarig: Christian Werme',
-    tasks: [
-      '•	Skickar ut enkät och sammanställer resultat 2 ggr per år ',
-    ]
-  },
-  {
-    heading: 'Marknad - Kund och event, synlighet',
-    subheading: 'Ansvarig: Sofia Flodmark',
-    tasks: [
-      '•	Marknadsföring av XLU',
-      '•	Delta på externa event'
     ]
   },
 ];
