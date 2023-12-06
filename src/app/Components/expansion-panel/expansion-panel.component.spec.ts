bf64e0e7c01570a274bf850b27db2762b55e0fc6
sohjla <sofia.hjort.labba@gmail.com>
2023-12-06 10:27:39 +0100

    Add tests

--- src/app/Components/expansion-panel/expansion-panel.component.spec.ts
+++ src/app/Components/expansion-panel/expansion-panel.component.spec.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonTask } from 'src/app/models/person-task.interface';
 
 describe('ExpansionPanelComponent', () => {
   let component: ExpansionPanelComponent;
@@ -22,5 +23,62 @@ describe('ExpansionPanelComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate progress for zero tasks to 0%', () => {
+    component.personTasks = [] as PersonTask[];
+    component.calculateTaskProgress();
+
+    expect(component.taskPercent).toEqual(0);
+  });
+
+  it('should calculate progress for one task to 0%', () => {
+    component.personTasks = [{ isCompleted: false }] as PersonTask[];
+    component.calculateTaskProgress();
+
+    expect(component.taskPercent).toEqual(0);
+  });
+
+  it('should calculate progress for multiple tasks to 0%', () => {
+    component.personTasks = [
+      { isCompleted: false },
+      { isCompleted: false },
+      { isCompleted: false },
+      { isCompleted: false }
+    ] as PersonTask[];
+    component.calculateTaskProgress();
+
+    expect(component.taskPercent).toEqual(0);
+  });
+
+  it('should calculate progress for multiple tasks to 25%', () => {
+    component.personTasks = [
+      { isCompleted: false },
+      { isCompleted: true },
+      { isCompleted: false },
+      { isCompleted: false }
+    ] as PersonTask[];
+    component.calculateTaskProgress();
+
+    expect(component.taskPercent).toEqual(25);
+  });
+
+  it('should calculate progress for one task to 100%', () => {
+    component.personTasks = [{ isCompleted: true }] as PersonTask[];
+    component.calculateTaskProgress();
+
+    expect(component.taskPercent).toEqual(100);
+  });
+
+  it('should calculate progress for multiple tasks to 100%', () => {
+    component.personTasks = [
+      { isCompleted: true },
+      { isCompleted: true },
+      { isCompleted: true },
+      { isCompleted: true }
+    ] as PersonTask[];
+    component.calculateTaskProgress();
+
+    expect(component.taskPercent).toEqual(100);
   });
 });
--- src/app/Containers/admin-page/admin-page.component.spec.ts
+++ src/app/Containers/admin-page/admin-page.component.spec.ts
@@ -16,10 +16,32 @@ import { ManageTasktypesComponent } from 'src/app/Components/manage-tasktypes/ma
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormErrorMessageComponent } from 'src/app/Components/form-error-message/form-error-message.component';
 import { MatIconModule } from '@angular/material/icon';
+import { Person } from 'src/app/models/person.interface';
+import { PersonService } from 'src/app/services/person.service';
+import { TaskService } from 'src/app/services/task.service';
+import { TaskTypeService } from 'src/app/services/tasktype.service';
+import { By } from '@angular/platform-browser';
+import { Task } from 'src/app/models/task.interface';
+import { TaskType } from 'src/app/models/task-type.interface';
+import { of } from 'rxjs';
 
 describe('AdminPageComponent', () => {
   let component: AdminPageComponent;
   let fixture: ComponentFixture<AdminPageComponent>;
+
+  let personService: PersonService;
+  let taskService: TaskService;
+  let taskTypeService: TaskTypeService;
+
+  let managePersonComponent: ManagePersonsComponent;
+  let managePersonFixture: ComponentFixture<ManagePersonsComponent>;
+
+  let manageTaskComponent: ManageTasksComponent;
+  let manageTaskFixture: ComponentFixture<ManageTasksComponent>;
+
+  let manageTasktypesComponent: ManageTasktypesComponent;
+  let manageTasktypesFixture: ComponentFixture<ManageTasktypesComponent>;
+  const taskTypeToHandle: TaskType = { id: 0 } as TaskType;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -66,17 +88,120 @@ describe('AdminPageComponent', () => {
               },
             },
           },
-        }
+        },
+        PersonService,
+        TaskService,
+        TaskTypeService
       ],
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(AdminPageComponent);
     component = fixture.componentInstance;
+
+    personService = TestBed.inject(PersonService);
+    spyOn(personService, 'create');
+    spyOn(personService, 'update');
+    spyOn(personService, 'remove');
+
+    taskService = TestBed.inject(TaskService);
+    spyOn(taskService, 'create');
+    spyOn(taskService, 'update');
+    spyOn(taskService, 'remove');
+
+    taskTypeService = TestBed.inject(TaskTypeService);
+    spyOn(taskTypeService, 'create').and.returnValue(of(taskTypeToHandle));
+    spyOn(taskTypeService, 'update');
+    spyOn(taskTypeService, 'delete');
+
+    managePersonFixture = TestBed.createComponent(ManagePersonsComponent);
+    manageTaskFixture = TestBed.createComponent(ManageTasksComponent);
+    manageTasktypesFixture = TestBed.createComponent(ManageTasktypesComponent);
+
     fixture.detectChanges();
+    managePersonFixture.detectChanges();
+    manageTaskFixture.detectChanges();
+    manageTasktypesFixture.detectChanges();
+
+    managePersonComponent = fixture.debugElement.query(By.directive(ManagePersonsComponent)).componentInstance;
+    manageTaskComponent = fixture.debugElement.query(By.directive(ManageTasksComponent)).componentInstance;
+    manageTasktypesComponent = fixture.debugElement.query(By.directive(ManageTasktypesComponent)).componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(managePersonComponent).toBeTruthy();
+    expect(manageTaskComponent).toBeTruthy();
+    expect(manageTasktypesComponent).toBeTruthy();
+  });
+
+  it('should handle person create', () => {
+    // Trigger the event output from ManagePersonComponent
+    const personToCreate: Person = { id: 0 } as Person;
+    managePersonComponent.createPerson.emit(personToCreate);
+
+    expect(personService.create).toHaveBeenCalledOnceWith(personToCreate);
+  });
+
+  it('should handle person update', () => {
+    // Trigger the event output from ManagePersonComponent
+    const personToUpdate: Person = { id: 0 } as Person;
+    managePersonComponent.updatePerson.emit(personToUpdate);
+
+    expect(personService.update).toHaveBeenCalledOnceWith(personToUpdate);
+  });
+
+  it('should handle person remove', () => {
+    // Trigger the event output from ManagePersonComponent
+    const personToRemove: Person = { id: 0 } as Person;
+    managePersonComponent.removePerson.emit(personToRemove.id);
+
+    expect(personService.remove).toHaveBeenCalledOnceWith(personToRemove.id);
+  });
+
+  it('should handle task create', () => {
+    // Trigger the event output from ManageTaskComponent
+    const taskToCreate: Task = { id: 0 } as Task;
+    manageTaskComponent.createTask.emit(taskToCreate);
+
+    expect(taskService.create).toHaveBeenCalledOnceWith(taskToCreate);
+  });
+
+  it('should handle task update', () => {
+    // Trigger the event output from ManageTaskComponent
+    const taskToUpdate: Task = { id: 0 } as Task;
+    manageTaskComponent.updateTask.emit(taskToUpdate);
+
+    expect(taskService.update).toHaveBeenCalledOnceWith(taskToUpdate);
+  });
+
+  it('should handle task remove', () => {
+    // Trigger the event output from ManageTaskComponent
+    const taskToRemove: Task = { id: 0 } as Task;
+    manageTaskComponent.removeTask.emit(taskToRemove.id);
+
+    expect(taskService.remove).toHaveBeenCalledOnceWith(taskToRemove.id);
+  });
+
+  it('should handle taskType create', () => {
+    // Trigger the event output from ManageTasktypesComponent
+    manageTasktypesComponent.createTaskType.emit(taskTypeToHandle);
+
+    expect(taskTypeService.create).toHaveBeenCalledOnceWith(taskTypeToHandle);
+    expect(component.taskTypes).toContain(taskTypeToHandle);
+  });
+
+  it('should handle taskType update', () => {
+    // Trigger the event output from ManageTasktypesComponent
+    manageTasktypesComponent.updateTaskType.emit(taskTypeToHandle);
+
+    expect(taskTypeService.update).toHaveBeenCalledOnceWith(taskTypeToHandle);
+  });
+
+  it('should handle taskType delete', () => {
+    // Trigger the event output from ManageTasktypesComponent
+    manageTasktypesComponent.deleteTaskType.emit(taskTypeToHandle.id);
+
+    expect(taskTypeService.delete).toHaveBeenCalledOnceWith(taskTypeToHandle.id);
   });
 });
